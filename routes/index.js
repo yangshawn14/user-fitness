a00075374e2bbc633d48fef199fd6c68b430b9af
Shawn Yang <yangshawn14@gmail.com>
2024-02-17 11:49:54 -0700

    Retest path for swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('users', require('./users'));
-router.use('workouts', require('./workouts'));
+router.use('/users', require('./users'));
+router.use('/workouts', require('./workouts'));
 
 module.exports = router;
