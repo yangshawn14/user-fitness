288c95f997148905c651e968a4e5e206492261f2
Shawn Yang <yangshawn14@gmail.com>
2024-02-17 10:47:14 -0700

    Push workouts to render

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./users'));
+router.use('/workouts', require('./workouts'));
 
 module.exports = router;
