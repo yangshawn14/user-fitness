3ff1440a4d6a348c9741b96b92660b715886fdfd
Shawn Yang <yangshawn14@gmail.com>
2024-02-16 20:01:49 -0700

    Add error handling to routes and server.js

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,10 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Get all users
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('users').find();
-  result.toArray().then((lists) => {
+  mongodb.getDb().db().collection('users').find().toArray((lists) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use((req, res, next) => {
 
 app.use('/', routes);
 
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
